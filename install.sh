e7f113cdc9b9651c6b650cedd857931aed1ec90e
ObsidianPresidium <96147683+ObsidianPresidium@users.noreply.github.com>
2021-12-28 14:55:04 +0100

    replace curls with working wgets

--- install.sh
+++ install.sh
@@ -2,10 +2,9 @@
 sudo apt install -y git
 git clone https://github.com/ObsidianPresidium/paper-pi
 cd paper-pi
-curl -o https://download.oracle.com/java/17/archive/jdk-17.0.1_linux-aarch64_bin.tar.gz
-tar -zxvf jdk-17.0.1_linux-aarch64_bin.tar.gz
-rm jdk-17.0.1_linux-aarch64_bin.tar.gz
-curl -o https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/103/downloads/paper-1.18.1-103.jar
-mv paper-1.18.1-103.jar paper.jar
+wget -O "jdk.tar.gz" https://download.oracle.com/java/17/archive/jdk-17.0.1_linux-aarch64_bin.tar.gz
+tar -zxvf jdk.tar.gz
+rm jdk.tar.gz
+wget -O "paper.jar" https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/103/downloads/paper-1.18.1-103.jar
 sudo chmod +x *
 echo Done
