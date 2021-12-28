d141ee086d33874f7c85d1157460e035ef5697ec
ObsidianPresidium <emil@aarestrup.nu>
2021-12-28 22:55:11 +0000

    Add aikars flags

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ tar -zxvf jdk.tar.gz
 rm jdk.tar.gz
 wget -O "paper.jar" https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/103/downloads/paper-1.18.1-103.jar
 echo "#!/bin/bash" > run.sh
-echo "$PWD/jdk-17.0.1/bin/java -Xmx$MEMALLOC -Xms$MEMALLOC -jar $PWD/paper.jar nogui" >> run.sh
+echo "$PWD/jdk-17.0.1/bin/java -Xmx$MEMALLOC -Xms$MEMALLOC -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $PWD/paper.jar nogui" >> run.sh
 sudo chmod +x *
 sudo chown $INSTALLER_USER *
 echo "Installation finished, you should be able to use 'sh run.sh' to launch the server now!"
