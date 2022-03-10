c7c17306df39d650e62e0c6d6060fb653b80ba01
Emil Aarestrup <emil@aarestrup.nu>
2022-03-10 21:18:11 +0100

    Update URLs, move to OpenJDK, switch to .desktop instead of .sh

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-INSTALLER_USER=`who am i | awk '{print $1}'`
+### --- These values are custom. They can be replaced with what you desire.
+MINECRAFT_SERVER_JAR=`https://papermc.io/api/v2/projects/paper/versions/1.18.2/builds/237/downloads/paper-1.18.2-237.jar`
+START_COMMAND=`paper-pi`
+### ---
+
 echo "+----------<{=====x=====}>----------+"
-echo "Welcome to paper-pi!"
+echo "Welcome to the paper-pi installation!"
 echo "Please specify how much RAM you want to allocate to the server (e.g. 2G for 2 gigabytes, 512M for 512 megabytes)":
+INSTALLER_USER=`who am i | awk '{print $1}'`
 read MEMALLOC </dev/tty
-sudo apt install -y git
+sudo apt install -y git openjdk-17-jre
 git clone https://github.com/ObsidianPresidium/paper-pi
-rm install.sh # remove this
+rm install.sh # remove myself
 cd paper-pi
-wget -O "jdk.tar.gz" https://download.oracle.com/java/17/archive/jdk-17.0.1_linux-aarch64_bin.tar.gz
-tar -zxvf jdk.tar.gz
-rm jdk.tar.gz
-wget -O "paper.jar" https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/103/downloads/paper-1.18.1-103.jar
-echo "#!/bin/bash" > run.sh
-echo "$PWD/jdk-17.0.1/bin/java -Xmx$MEMALLOC -Xms$MEMALLOC -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $PWD/paper.jar nogui" >> run.sh
-sudo chmod +x *
-sudo chown $INSTALLER_USER *
-echo "Installation finished, you should be able to use 'sh run.sh' to launch the server now!"
+wget -O "paper.jar" $MINECRAFT_SERVER_JAR
+echo "alias $START_COMMAND=java -Xmx$MEMALLOC -Xms$MEMALLOC -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $PWD/paper.jar nogui" >> ~/.bashrc
+sudo chown -R $INSTALLER_USER *
+echo "Installation finished, you should be able to use the shortcut to launch the server now! (you can also start the server manually by using the paper-pi command in a terminal window)"
